fe25b037aa54f25ff50b5908c7f8b45710648fc3
bjlli <brendajacomelli@gmail.com>
2023-01-26 15:47:58 -0300

    melhorando overlay generator

--- pshBtnsDriver/overlayGen.sh
+++ pshBtnsDriver/overlayGen.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
-echo "/dts-v1/;" > overlay-$1.dts
-echo "/plugin/;" >> overlay-$1.dts
-echo "/{" >> overlay-$1.dts
-echo "	compatible = \"allwinner,sun8i-h3\";" >> overlay-$1.dts
-echo "	fragment@0 {" >> overlay-$1.dts
-echo "		target-path = \"/\";" >> overlay-$1.dts
-echo "		__overlay__{" >> overlay-$1.dts
-echo "			my_device_$1{" >> overlay-$1.dts
-echo "				dev_num = <$1>;" >> overlay-$1.dts
-echo "				compatible = \"emc-logic, pshBtns\";" >> overlay-$1.dts
-echo "				status = \"okay\";" >> overlay-$1.dts
-echo "				pBtn_label = \"rpi-gpio-$2\";" >> overlay-$1.dts
-echo "				pBtn_gpio = <$2>;" >> overlay-$1.dts
-echo "			};" >> overlay-$1.dts
-echo "		};" >> overlay-$1.dts
-echo "	};" >> overlay-$1.dts
-echo "};" >> overlay-$1.dts
-armbian-add-overlay overlay-$1.dts
+
+declare -i i=0
+
+while [[ $i -le 6 ]]
+do
+    echo $i
+    if [[ -f "overlay-$i.dts" ]]
+    then
+        echo "File overlay-$i.dts exists"
+    else
+        echo "File  overlay-$i.dts does not exist"
+        break
+    fi
+    ((i++))
+done
+
+echo "/dts-v1/;" > overlay-$i.dts
+echo "/plugin/;" >> overlay-$i.dts
+echo "/{" >> overlay-$i.dts
+echo "	compatible = \"allwinner,sun8i-h3\";" >> overlay-$i.dts
+echo "	fragment@0 {" >> overlay-$i.dts
+echo "		target-path = \"/\";" >> overlay-$i.dts
+echo "		__overlay__{" >> overlay-$i.dts
+echo "			my_device_$i{" >> overlay-$i.dts
+echo "				dev_num = <$i>;" >> overlay-$i.dts
+echo "				compatible = \"emc-logic, pshBtns\";" >> overlay-$i.dts
+echo "				status = \"okay\";" >> overlay-$i.dts
+echo "				pBtn_label = \"rpi-gpio-$1\";" >> overlay-$i.dts
+echo "				pBtn_gpio = <$1>;" >> overlay-$i.dts
+echo "			};" >> overlay-$i.dts
+echo "		};" >> overlay-$i.dts
+echo "	};" >> overlay-$i.dts
+echo "};" >> overlay-$i.dts
+armbian-add-overlay overlay-$i.dts
